5bbcaaab769ac0993b68612fa79db7612523febf
Emanuelle Garcia Guedes <garcia.emanuelle@academico.ifpb.edu.br>
2024-05-02 13:42:18 -0300

    configurando o cors

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/emanuelleGued/go-rest-api/database"
 	"github.com/emanuelleGued/go-rest-api/models"
+	"github.com/gorilla/mux"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -13,5 +15,43 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(models.Personalidades)
+	// w.Header().Set("Content-type", "application/json") // deixar o codigo no formato json
+	var p []models.Personalidade
+	database.DB.Find(&p)
+	json.NewEncoder(w).Encode(p)
+}
+
+func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // vars é usado para obter o valor do parametro id
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
+
+func CriaUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	var novaPersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&novaPersonalidade)
+	database.DB.Create(&novaPersonalidade)
+	json.NewEncoder(w).Encode(novaPersonalidade)
+
+}
+
+func DeletaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.Delete(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+
+}
+func EditarPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id) // o banco de dados vai encontrar a personalidade
+	json.NewDecoder(r.Body).Decode(&personalidade)
+	database.DB.Save(&personalidade)
+	json.NewEncoder(w).Encode(personalidade)
+
 }
--- routes/routes.go
+++ routes/routes.go
@@ -5,10 +5,19 @@ import (
 	"net/http"
 
 	"github.com/emanuelleGued/go-rest-api/controllers"
+	"github.com/emanuelleGued/go-rest-api/middleware"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditarPersonalidade).Methods("Put")
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
